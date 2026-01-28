118a66e3f553fa9d4a87934ea969e7ee66bb5907
EllaGLG <gabriela.llanos@ucas-edu.net>
2026-01-28 14:42:40 -0700

    silly-string

---
+++ Practices/silly_sentence.py
@@ -0,0 +1,11 @@
+#GLG silly_sentences.py
+
+place = input("Enter a place: ")
+
+verb = input("Enter a verb: ")
+
+feeling = input("Enter a felling: ")
+
+verb2 = input("Enter a verb: ")
+
+print("At the", place, "in the sun we'll be",verb,"and having fun and it makes me feel so",feeling,"I", verb2, ".")
