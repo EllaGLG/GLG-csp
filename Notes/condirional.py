5e9ebe31a5dc6218d93227420372a55cff93b800
EllaGLG <gabriela.llanos@ucas-edu.net>
2026-01-30 14:29:43 -0700

    time of day

--- Notes/condirional.py
+++ Notes/condirional.py
@@ -7,13 +7,13 @@ print("end of the program")
 name = ("hat is your name").srip().capitalize()
 
 if name == "Arthur":
-    print ("welcome king Arthur.")
-    quest == "Holy Grail":
+    print("welcome king Arthur.")
+    quest == "Holy Grail"
     print("you may pass. Good luck!")
 else:
     print(f"{name} is a lame quest. Good Luck!")
-else:
-print(F"{quest}is a lame quest. Get goode.")
+#else:
+#print(f"{quest}is a lame quest. Get goode.")
 
 
 
---
+++ Practices/time_of_day.py
@@ -0,0 +1,7 @@
+#GLG time_of_day.py
+
+time = input('what time is it?')
+
+if time >= 1200 : 
+    
+    print(int("good morning"))
