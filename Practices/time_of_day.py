738190f298890d86e4c1d73831de77d870c65208
EllaGLG <gabriela.llanos@ucas-edu.net>
2026-01-30 14:41:39 -0700

    issac boo

--- Practices/time_of_day.py
+++ Practices/time_of_day.py
@@ -1,7 +1,11 @@
 #GLG time_of_day.py
 
-time = input('what time is it?')
+time = int(imput(what time is it?"))
 
-if time >= 1200 : 
+if time<= 1200 : 
     
-    print(int("good morning"))
+    print("good morning")
+ elif age >= 1200 and age <1900:
+    Print("good evening")
+else:
+   print("good nigth.")
