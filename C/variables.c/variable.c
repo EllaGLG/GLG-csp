81e1af6cea35529ba28149472f2e8bcea3bd8248
EllaGLG <gabriela.llanos@ucas-edu.net>
2026-02-19 14:42:17 -0700

    variable practise

--- C/variables.c/variable.c
+++ C/variables.c/variable.c
@@ -1,7 +1,27 @@
 // This is a comment in C!
 #include <stdio.h> // Lets us use inputs and outputs 
 
+
 int main(){
-    printf("It work");
-    return 0; // required last line 
+    //variables examples
+    int age = 9999;
+    const float gpa = 3.98;
+    char grade = 'A';  // single letter uses single quotes
+    char name[] = "Erick";
+    int number;
+    char user[50];
+
+    printf("tell me your name\n");
+    scanf("%s", &user);
+
+    printf("what is your fav number?");
+    scanf("%d", &number);
+
+    printf("your  number is: %d\n", user, number);
+    printf("It worked!\n");
+    printf("%d\n", age);
+    printf("your GPA is %f\n", gpa);
+     printf("you  have a %c incomputer science\n", grade);
+     return 0; //required last line in your main funtion
+
 }
