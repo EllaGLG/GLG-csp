7c397ef1f5ee5cdd181341bdb1292c2571acf645
EllaGLG <gabriela.llanos@ucas-edu.net>
2026-02-20 16:06:31 -0700

    first procject'

---
+++ C/variables.c/first_project.c
@@ -0,0 +1,14 @@
+//LG period 7th first_procject
+
+#include <stdio.h>
+
+int main(){
+   
+   char user[50];
+
+   printf("tell me your name\n");
+   scanf("%s", &user);
+   printf(" welcome to your vs %s\n", user);
+
+    return 0;
+}
